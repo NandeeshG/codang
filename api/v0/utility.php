0118da196bd64f5f143a6c602ff3368b8468af3c
NandeeshG <nandeeshgupta@gmail.com>
2020-11-25 04:59:29 +0530

    set of utility functions not specific to any one file are here

---
+++ api/v0/utility.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once 'error.php';
+
+function isCli()
+{
+    if (strcmp(php_sapi_name(), 'cli')===0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+function newline($str="")
+{
+    $endl = isCli()===1 ? "\n" : "<br/>";
+    return $str . $endl;
+}
+
+function checkWaitError($str)
+{
+    if (strcmp($str, 'API request limit exhausted')===0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function extractError($response)
+{
+    if (strcmp($response['status'], "OK")===0||strcmp($response['status'], "ok")===0) {
+        return "ok";
+    } elseif ($response['result']['errors'][0]['message']) {
+        return $response['result']['errors'][0]['message'];
+    } elseif ($response['result']['errors']['message']) {
+        return $response['result']['errors']['message'];
+    } else {
+        logError("Error not extracted!! ".json_decode($response), $response);
+        return "ok";
+    }
+}
